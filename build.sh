6e142e6da59db0771406d2f1040a078bdc29db29
Josia <josia-internet@rosskopfs.de>
2019-01-29 17:06:23 +0100

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
+#!/bin/bash
 cd "`dirname "$0"`"
-pushd ubuntu >/dev/null
-bash build.sh
-popd >/dev/null
 git pull --rebase upstream master || exit
 git push
 docker build --no-cache -t sameersbn/apt-cacher-ng .
